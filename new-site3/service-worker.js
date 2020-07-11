328062ecdd4851f789e5d7ae0d0dd8e0b3082774
Gautham Sajith <gsajith@umich.edu>
2020-07-11 17:48:09 -0400

    Update new site

--- new-site3/service-worker.js
+++ new-site3/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f91c3c1dec6cfb326303a2d875861812.js"
+  "/gsajith_new_site/precache-manifest.6cee5016526589bccf02299dbef7c31c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gsajith_new_site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
